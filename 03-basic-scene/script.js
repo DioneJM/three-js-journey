b806105f2b791362e9627351ea33f6e66b6060fc
Dione <dionemorales@outlook.com>
2022-03-07 02:36:00 +1100

    first render

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,1 +1,27 @@
-console.log('hello three.js')
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color: 'red' })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+const fov = 75
+const sizes = {
+    width: 800,
+    height: 600
+}
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio)
+camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
