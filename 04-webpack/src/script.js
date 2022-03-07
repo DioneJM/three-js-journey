51a5b13d2cc8841a2262e32d8fa027e6de174b83
Dione <dionemorales@outlook.com>
2022-03-08 06:55:49 +1100

    created same project but using webpack

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,1 +1,25 @@
 import './style.css'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+const fov = 75;
+const sizes = {
+    width: 800,
+    height: 600
+}
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio)
+camera.position.z = 3
+
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
